63b9d19af6ffadec0974e0f5be8b73f4e3abea95
Peter Fiorella <PTRFRLL@gmail.com>
2018-07-01 00:04:19 -0600

    Fixed hardcoded paths in tests

--- tests/discord.test.js
+++ tests/discord.test.js
@@ -7,13 +7,13 @@ const removeFile = require('../lib/utils.js');
 
 describe('Compare.js', () => {
 	it('should get compare score > 10,000', (done) => {
-		compare('/home/peter/node-discord-bot/tests/unknown.png').then((score) => {
+		compare(__dirname + '/../tests/unknown.png').then((score) => {
 			expect(score).toBeGreaterThan(10000);
 			done();
 		});
 	});
 	it('should get compare score < 10,000', (done) => {
-		compare('/home/peter/node-discord-bot/tests/unknown720.png').then((score) => {
+		compare(__dirname+ '/../tests/unknown720.png').then((score) => {
 			expect(score).toBeLessThan(10000);
 			done();
 		});
@@ -23,7 +23,7 @@ describe('Compare.js', () => {
 describe('Download.js', () => {
 	it('should download a file', (done) => {
 		download('https://peterfiorella.com/img/DinnerBot/dinner.png').then((path) => {
-			expect(path).toBe('/home/peter/node-discord-bot/lib/../data/img/downloads/temp.png');
+			expect(fs.existsSync(path)).toBe(true);
 			done();
 		});
 	});
@@ -31,8 +31,9 @@ describe('Download.js', () => {
 
 describe('Utils.js', () => {
 	it('should delete a file', (done) => {
-		removeFile('/home/peter/node-discord-bot/data/img/downloads/temp.png');
-		fs.stat('/home/peter/node-discord-bot/data/img/downloads/temp.png', (err, stat) => {
+		let path = __dirname + '/../data/img/downloads/temp.png';
+		removeFile(path);
+		fs.stat(path, (err, stat) => {
 			expect(err.code).toBe('ENOENT');
 			done();
 		});
