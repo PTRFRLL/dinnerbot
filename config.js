3827af00e447e2e90c76395f35f3a6f9e128401f
Peter Fiorella <git@fiorella.me>
2021-04-12 20:36:31 -0600

    Remove responses from config

--- config.js
+++ config.js
@@ -1,38 +1,6 @@
 const env = process.env.NODE_ENV || 'DOCKER';
 const path = require('path');
 
-let good = [
-	'Whatever you say...', 
-	`You're the boss!`, 
-	`Ok, fine!`, 
-	`:shrug:`
-];
-let bad = [
-	':middle_finger:', 
-	'Who are you again?', 
-	':poop:', 
-	'How bout you drink a nice glass of shut the hell up :coffee:', 
-	'Could you fucking not?', 
-	`You're stupid and I hate you`
-];
-
-//[USER] is replaced by user(s) mention e.g. @PTRFRLL
-let winner = [
-	'Do I smell chicken :chicken:, [USER]?', 
-	'What\'s that smell, [USER]?', 
-	'Mmm, something smells good [USER]', 
-	'[USER], did you order Popeyes :chicken:?', 
-	'[USER] Looks like we got some winners over here :fork_knife_plate:',
-	'[USER] Look at these fucking guys :eyes:'
-];
-let spectator = [
-	'Couldn\'t make it to the end, [USER]? :skull_crossbones:', 
-	'At least you\'re light, [USER] :stuck_out_tongue_winking_eye:', 
-	'[USER] gettin carried per usual...', 
-	'[USER] for MVP :smiling_imp:', 
-	'[USER], there for moral support... :angel:'
-];
-
 const dev = {
 	app: {
 		DISCORD_BOT_TOKEN: 'DISCORD_BOT_TOKEN',
@@ -45,10 +13,6 @@ const dev = {
 			users: [''], //user ids
 			roles: [''] //name of role
 		},
-		BOT_RESPONSES_GOOD: good,
-		BOT_RESPONSES_BAD: bad,
-		BOT_PRESENCE_RESPONSES: winner,
-		BOT_SPECTATOR_RESPONSES: spectator,
 		COMMAND_PREFIX: '!',
 		STAT_WAIT_TIME_IN_SECONDS: 100,
 		STAT_RETRY_TIME_IN_SECONDS: 30,
@@ -74,10 +38,6 @@ const DOCKER = {
 			users: [''], //user ids
 			roles: [''] //name of role
 		},
-		BOT_RESPONSES_GOOD: good,
-		BOT_RESPONSES_BAD: bad,
-		BOT_PRESENCE_RESPONSES: winner,
-		BOT_SPECTATOR_RESPONSES: spectator,
 		COMMAND_PREFIX: '!',
 		STAT_WAIT_TIME_IN_SECONDS: 100, //how long to wait before querying stats
 		STAT_RETRY_TIME_IN_SECONDS: 30, //how often to retry query for stats
